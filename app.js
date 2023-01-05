a5b796ee81fcc63edeade2ec65d079ef53050607
agustinaFerrada <agustinagriselrodriguezferrada@gmail.com>
2023-01-05 19:59:11 -0300

    inicializo proyecto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+
+app.listen(PORT, ()=> console.log(`
+El servidor esta levantado ${PORT}
+http://localhost:${PORT}
+`))
